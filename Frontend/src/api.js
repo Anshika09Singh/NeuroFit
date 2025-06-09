d82ac7f85d21b063e4e5fd30e3393c65d1c57e68
Anshika Singh <157716478+Anshika09Singh@users.noreply.github.com>
2025-06-09 12:05:31 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 export const saveProgress = async (progress) => {
   try {
-    const response = await fetch("${https://neuro-fit-64ts.vercel.app/}/save-progress", {
+    const response = await fetch(`https://neuro-fit-64ts.vercel.app/save-progress`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
